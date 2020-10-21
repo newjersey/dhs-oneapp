b43d87851961cb879a495bdab57beb82abc8a222
Ron Heft <Ronald.K.Heft@omb.eop.gov>
2020-10-21 15:12:51 -0400

    fix: parsing booleans

--- oneapp-server/email.config.js
+++ oneapp-server/email.config.js
@@ -1,6 +1,8 @@
 const nodemailer = require('nodemailer');
 const config = require('config');
 const { get } = require('lodash');
+const yn = require('yn');
+const logger = require('./logger.config');
 
 let transporter;
 
@@ -11,19 +13,20 @@ const createTransporter = async () => {
   }
 
   // Create a test account on ethereal.email when using debug mode
-  const debug = config.get('smtp.debug');
+  const debug = yn(config.get('smtp.debug'));
   const testAccount = debug ? await nodemailer.createTestAccount() : undefined;
-
-  return nodemailer.createTransport({
+  const options = {
     host: config.get('smtp.host'),
     port: config.get('smtp.port'),
-    secure: config.get('smtp.secure'),
+    secure: yn(config.get('smtp.secure')),
     auth: {
       // Authenticate with the testAccount, if created, otherwise use the config value
       user: get(testAccount, 'user', config.get('smtp.user')),
       pass: get(testAccount, 'pass', config.get('smtp.pass')),
     },
-  });
+  };
+  logger.info(options, 'Creating nodemailer transporter');
+  return nodemailer.createTransport(options);
 };
 
 module.exports = { createTransporter };
