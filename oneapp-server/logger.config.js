40efc2d8c145a38f24102e7f0269274fa10e8d83
Ron Heft <Ronald.K.Heft@omb.eop.gov>
2020-10-21 15:11:51 -0400

    chore: Improve logging and redact sensitive logs

--- oneapp-server/logger.config.js
+++ oneapp-server/logger.config.js
@@ -1,8 +1,16 @@
 const pino = require('pino');
 
 const logger = pino({
+  name: 'oneapp-server',
   level: process.env.LOG_LEVEL || 'info',
   enabled: !(process.env.LOG_ENABLED === 'false'),
+  // Remove sensitive information from logs
+  redact: {
+    paths: [
+      'auth.user',
+      'auth.pass',
+    ],
+  },
 });
 
 module.exports = logger;
