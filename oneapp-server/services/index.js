9cb1c87cb83631213a54901b4907a95a844714a3
Ron Heft <Ronald.K.Heft@omb.eop.gov>
2020-10-22 16:18:15 -0400

    Fix: Not all services included in TestClient

--- oneapp-server/services/index.js
+++ oneapp-server/services/index.js
@@ -1,1 +1,6 @@
-module.exports = require('require-directory')(module, { recurse: false, exclude: /\.test\.js/ });
+const { assign } = require('lodash');
+
+const services = require('require-directory')(module, { recurse: false, exclude: /\.test\.js/ });
+const mockServices = require('require-directory')(module, './__mocks__', { recurse: false, exclude: /\.test\.js/ });
+
+module.exports = assign({}, services, mockServices);
