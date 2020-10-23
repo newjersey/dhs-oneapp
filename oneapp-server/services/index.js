f6188b5e1d0dbf1efa25455cfb66d1d4b9eeb2b9
Ron Heft <Ronald.K.Heft@omb.eop.gov>
2020-10-23 10:55:10 -0400

    fix: mock services import for tests

--- oneapp-server/services/index.js
+++ oneapp-server/services/index.js
@@ -1,6 +1,1 @@
-const { assign } = require('lodash');
-
-const services = require('require-directory')(module, { recurse: false, exclude: /\.test\.js/ });
-const mockServices = require('require-directory')(module, './__mocks__', { recurse: false, exclude: /\.test\.js/ });
-
-module.exports = assign({}, services, mockServices);
+module.exports = require('require-directory')(module, { recurse: false, exclude: /\.test\.js/ });
