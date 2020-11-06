af69138b68df39bd545530f3012b0fc4712a102d
Ron Heft <Ronald.K.Heft@omb.eop.gov>
2020-11-06 11:45:31 -0500

    297: First pass at defining application contact fields

--- oneapp-server/types/ApplicationContact.js
+++ oneapp-server/types/ApplicationContact.js
@@ -7,7 +7,35 @@ const typeDef = gql`
   }
 
   type ApplicationContact {
-    APPLICATION_FIRST_NAME: String
+    APPLICANT_FIRST_NAME: String
+    APPLICANT_LAST_NAME: String
+    APPLICANT_MIDDLE_NAME: String
+    APPLICANT_MAIDEN_NAME: String
+    PAYEE_FIRST_NAME: String
+    PAYEE_LAST_NAME: String
+    IS_HOMELESS: Boolean
+    ADDRESS1: String
+    ADDRESS2: String
+    CITY: String
+    STATE: String
+    ZIP: Int
+    ZIP4: Int
+    COUNTY_NUMBER: Int
+    M_ADDRESS1: String
+    M_ADDRESS2: String
+    M_CITY: String
+    M_STATE: String
+    M_ZIP: Int
+    M_ZIP4: Int
+    HOME_PHONE_NUMBER: Int
+    WORK_PHONE_NUMBER: Int
+    CELL_PHONE_NUMBER: Int
+    OTHER_PHONE_NUMBER: Int
+    EMAIL_ADDRESS: String
+    NO_PHONE_NUMBER: Boolean
+    NO_CONTACT_INFORMATION: Boolean
+    APPLICATION_TYPE: Int
+    APPLICATION_SUBTYPE: Int
   }
 `;
 
