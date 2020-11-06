8453bac5d813d5cbc8611c34a8bdaf3e1abb78bf
Ron Heft <Ronald.K.Heft@omb.eop.gov>
2020-11-06 11:52:42 -0500

    297: First pass at defining info fields

--- oneapp-server/types/ApplicationFoodStampInfo.js
+++ oneapp-server/types/ApplicationFoodStampInfo.js
@@ -7,6 +7,12 @@ const typeDef = gql`
 
   type ApplicationFoodStampInfo {
     IS_GROSS_INCOME_LT_150: Boolean
+    IS_RENT_GT_GROSS_INCOME: Boolean
+    HAS_MIGRANT_FARM_WORKER: Boolean
+    HAS_RECEIVED_EMERGENCY_FS: Boolean
+    EMERGENCY_FS_DATE: Date
+    EMERGENCY_FS_LOCATION: String
+    EMERGENCY_FS_STATE: String
   }
 `;
 
--- oneapp-server/types/ApplicationProgramInfo.js
+++ oneapp-server/types/ApplicationProgramInfo.js
@@ -7,6 +7,21 @@ const typeDef = gql`
 
   type ApplicationProgramInfo {
     IS_FS_SELECTED: Boolean
+    IS_TF_SELECTED: Boolean
+    IS_GA_SELECTED: Boolean
+    HAVE_ACTIVE_CASE_CURRENTLY: Boolean
+    CURRENT_CASE_NUMBERS: String
+    HAD_ACTIVE_CASE_PREVIOULSY: Boolean
+    PREVIOUS_CASE_NUMBERS: String
+    SPOKEN_LANGUAGE: String
+    HAS_PAYEE: Boolean
+    NEED_ACCOMODATION: Boolean
+    NEED_ACM_TRANSLATOR: Boolean
+    NEED_ACM_SIGNING: Boolean
+    NEED_ACM_VISUALLY_IMPAIRED: Boolean
+    NEED_ACM_OTHER: Boolean
+    ACM_TRA_LANGUAGE: String
+    ACM_OTH_DESCRIPTION: String
   }
 `;
 
