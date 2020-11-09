5a4d2f42fc2833341dace94b972ff3a56422b1a1
Ron Heft <Ronald.K.Heft@omb.eop.gov>
2020-11-09 12:28:10 -0500

    #297: Rough structure for application mutation

--- oneapp-server/types/ApplicationContact.js
+++ oneapp-server/types/ApplicationContact.js
@@ -67,6 +67,59 @@ const typeDef = gql`
     APPLICATION_SUBTYPE: ApplicationSubtype
   }
 
+  input ApplicationContactInput {
+    "Applicant first name"
+    APPLICANT_FIRST_NAME: String
+    "Applicant last name"
+    APPLICANT_LAST_NAME: String
+    "Applicant middle name"
+    APPLICANT_MIDDLE_NAME: String
+    "Application maiden name"
+    APPLICANT_MAIDEN_NAME: String
+    "Payee first name, if different than applicant"
+    PAYEE_FIRST_NAME: String
+    "Payee last name, if different than applicant"
+    PAYEE_LAST_NAME: String
+    "Is applicant homeless"
+    IS_HOMELESS: Boolean
+    "Applicant address line 1"
+    ADDRESS1: String
+    "Applicant address line 2"
+    ADDRESS2: String
+    "Applicant address city"
+    CITY: String
+    "Applicant address state"
+    STATE: String
+    "Applicant address zipcode"
+    ZIP: Int
+    "Applicant address zipcode extend 4"
+    ZIP4: Int
+    "New Jersey county id, retrieved from zipcode to county lookup"
+    COUNTY_NUMBER: Int
+    "Mailing address line 1"
+    M_ADDRESS1: String
+    "Mailing address line 2"
+    M_ADDRESS2: String
+    "Mailing address city"
+    M_CITY: String
+    "Mailing address state"
+    M_STATE: String
+    "Mailing address zipcode"
+    M_ZIP: Int
+    "Mailing address zipcode extended 4"
+    M_ZIP4: Int
+    "Applicant home phone number"
+    HOME_PHONE_NUMBER: Int
+    "Applicant work phone number"
+    WORK_PHONE_NUMBER: Int
+    "Applicant cell phone number"
+    CELL_PHONE_NUMBER: Int
+    "Applicant other phone number"
+    OTHER_PHONE_NUMBER: Int
+    "Applicant email address"
+    EMAIL_ADDRESS: String
+  }
+
   enum ApplicationType {
     "OneApp"
     OA
