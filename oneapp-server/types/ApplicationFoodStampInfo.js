f1a627e3c5f9d51d95218d91e326cc50c4a8b5a4
Maya <maya@USDSs-MBP.fios-router.home>
2020-11-13 14:15:46 -0500

    fix issues

--- oneapp-server/types/ApplicationFoodStampInfo.js
+++ oneapp-server/types/ApplicationFoodStampInfo.js
@@ -6,10 +6,27 @@ const typeDef = gql`
   }
 
   extend type Mutation {
-    foodStampInfoUpdate(input: ApplicationFoodStampInfo!): ApplicationFoodStampInfo
+    foodStampInfoUpdate(input: ApplicationFoodStampInfoInput!): ApplicationFoodStampInfo
   }
 
   type ApplicationFoodStampInfo {
+    "Monthly household income is less than $150 a month"
+    IS_GROSS_INCOME_LT_150: Boolean
+    "Rent is greater than applicant gross income"
+    IS_RENT_GT_GROSS_INCOME: Boolean
+    "Applicant household has migrant farm worker"
+    HAS_MIGRANT_FARM_WORKER: Boolean
+    "Household is receiving emergency foodstamps"
+    HAS_RECEIVED_EMERGENCY_FS: Boolean
+    "Household received emergency foodstamps on date"
+    EMERGENCY_FS_DATE: Date
+    "Household received emergency foodstamps in city"
+    EMERGENCY_FS_LOCATION: String
+    "Household received emergency foodstamps in state"
+    EMERGENCY_FS_STATE: String
+  }
+
+  input ApplicationFoodStampInfoInput {
     "Monthly household income is less than $150 a month"
     IS_GROSS_INCOME_LT_150: Boolean
     "Rent is greater than applicant gross income"
