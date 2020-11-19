d4eb2b393cdea896262cc4624b4e40e13285da45
Maya <maya@USDSs-MBP.fios-router.home>
2020-11-19 09:18:37 -0500

    remove separate food stamp mutation

--- oneapp-server/types/ApplicationFoodStampInfo.js
+++ oneapp-server/types/ApplicationFoodStampInfo.js
@@ -5,10 +5,6 @@ const typeDef = gql`
     foodStampInfo: ApplicationFoodStampInfo
   }
 
-  extend type Mutation {
-    foodStampInfoUpdate(input: ApplicationFoodStampInfoInput!): ApplicationFoodStampInfo
-  }
-
   type ApplicationFoodStampInfo {
     "Monthly household income is less than $150 a month"
     IS_GROSS_INCOME_LT_150: Boolean
