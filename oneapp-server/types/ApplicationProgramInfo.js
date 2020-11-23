bd36833951c6d83e77e372f1bae34407152ed8af
Maya <maya@USDSs-MBP.fios-router.home>
2020-11-23 15:38:04 -0500

    refactor program info mutation atop application

--- oneapp-server/types/ApplicationProgramInfo.js
+++ oneapp-server/types/ApplicationProgramInfo.js
@@ -13,25 +13,25 @@ const typeDef = gql`
     "Applying for General Assistance"
     IS_GA_SELECTED: Boolean
     "Has active SNAP case"
-    HAVE_ACTIVE_CASE_CURRENTLY: Boolean
+    HAVE_ACTIVE_CASE_CURRENTLY: Boolean!
     "Case number(s) of active SNAP applications"
     CURRENT_CASE_NUMBERS: String
     "Has received SNAP previously"
-    HAD_ACTIVE_CASE_PREVIOULSY: Boolean
+    HAD_ACTIVE_CASE_PREVIOULSY: Boolean!
     "Case number(s) of previous SNAP applications"
     PREVIOUS_CASE_NUMBERS: String
     "Spoken language of applicant"
-    SPOKEN_LANGUAGE: String
+    SPOKEN_LANGUAGE: String!
     "Applicant needs accommodation"
-    NEED_ACCOMODATION: Boolean
+    NEED_ACCOMODATION: Boolean!
     "Needs accommodation type translator"
-    NEED_ACM_TRANSLATOR: Boolean
+    NEED_ACM_TRANSLATOR: Boolean!
     "Needs accommodation type signing"
-    NEED_ACM_SIGNING: Boolean
+    NEED_ACM_SIGNING: Boolean!
     "Needs accommodation type visual"
-    NEED_ACM_VISUALLY_IMPAIRED: Boolean
+    NEED_ACM_VISUALLY_IMPAIRED: Boolean!
     "Needs accommodation type other"
-    NEED_ACM_OTHER: Boolean
+    NEED_ACM_OTHER: Boolean!
     "Accommodation translator language"
     ACM_TRA_LANGUAGE: String
     "Description of other accommodation type"
@@ -46,25 +46,25 @@ const typeDef = gql`
     "Applying for General Assistance"
     IS_GA_SELECTED: Boolean
     "Has active SNAP case"
-    HAVE_ACTIVE_CASE_CURRENTLY: Boolean
+    HAVE_ACTIVE_CASE_CURRENTLY: Boolean!
     "Case number(s) of active SNAP applications"
     CURRENT_CASE_NUMBERS: String
     "Has received SNAP previously"
-    HAD_ACTIVE_CASE_PREVIOULSY: Boolean
+    HAD_ACTIVE_CASE_PREVIOULSY: Boolean!
     "Case number(s) of previous SNAP applications"
     PREVIOUS_CASE_NUMBERS: String
     "Spoken language of applicant"
-    SPOKEN_LANGUAGE: String
+    SPOKEN_LANGUAGE: String!
     "Applicant needs accommodation"
-    NEED_ACCOMODATION: Boolean
+    NEED_ACCOMODATION: Boolean!
     "Needs accommodation type translator"
-    NEED_ACM_TRANSLATOR: Boolean
+    NEED_ACM_TRANSLATOR: Boolean!
     "Needs accommodation type signing"
-    NEED_ACM_SIGNING: Boolean
+    NEED_ACM_SIGNING: Boolean!
     "Needs accommodation type visual"
-    NEED_ACM_VISUALLY_IMPAIRED: Boolean
+    NEED_ACM_VISUALLY_IMPAIRED: Boolean!
     "Needs accommodation type other"
-    NEED_ACM_OTHER: Boolean
+    NEED_ACM_OTHER: Boolean!
     "Accommodation translator language"
     ACM_TRA_LANGUAGE: String
     "Description of other accommodation type"
