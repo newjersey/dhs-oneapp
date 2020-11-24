af837ca522a7d1fc75dd48dda43dc380b221eb17
Maya <maya@USDSs-MBP.fios-router.home>
2020-11-24 11:08:01 -0500

    refactor to eligibility impl function

--- oneapp-server/dao/EligibilityCalcsDao.js
+++ oneapp-server/dao/EligibilityCalcsDao.js
@@ -3,51 +3,23 @@ const oracledb = require('oracledb');
 
 class EligibilityCalcsDao extends SQLDataSource {
   async getEligibility(APPLICATION_ID, LANGUAGE) {
-    const con = await this.knex.client.pool.acquire().promise;
-    const response = await this.knex.client.transaction(async (tx) => {
-      const bindVars = {
-        app_id: { dir: oracledb.BIND_IN, val: APPLICATION_ID },
-        lang: { dir: oracledb.BIND_IN, val: LANGUAGE },
-        eligibility: { dir: oracledb.BIND_OUT, type: oracledb.DB_TYPE_CURSOR },
-      };
-      return tx.raw('begin OA_PKG_UTIL.GET_ELIGIBILITY(:app_id, :lang, :eligibility); end;', bindVars);
-    }, { connection: con });
-    const result = await response[0].getRow();
-    response[0].close();
-    return result;
+    return await this.getEligibilityImpl('begin OA_PKG_UTIL.GET_ELIGIBILITY(:app_id, :lang, :eligibility); end;', APPLICATION_ID, LANGUAGE);
   }
 
   async getFcEligibility(APPLICATION_ID, LANGUAGE) {
-    const con = await this.knex.client.pool.acquire().promise;
-    const response = await this.knex.client.transaction(async (tx) => {
-      const bindVars = {
-        app_id: { dir: oracledb.BIND_IN, val: APPLICATION_ID },
-        lang: { dir: oracledb.BIND_IN, val: LANGUAGE },
-        eligibility: { dir: oracledb.BIND_OUT, type: oracledb.DB_TYPE_CURSOR },
-      };
-      return tx.raw('begin OA_PKG_UTIL.GET_FC_ELIGIBILITY(:app_id, :lang, :eligibility); end;', bindVars);
-    }, { connection: con });
-    const result = await response[0].getRow();
-    response[0].close();
-    return result;
+    return await this.getEligibilityImpl('begin OA_PKG_UTIL.GET_FC_ELIGIBILITY(:app_id, :lang, :eligibility); end;', APPLICATION_ID, LANGUAGE);
+
   }
 
   async getTfEligibility(APPLICATION_ID, LANGUAGE) {
-    const con = await this.knex.client.pool.acquire().promise;
-    const response = await this.knex.client.transaction(async (tx) => {
-      const bindVars = {
-        app_id: { dir: oracledb.BIND_IN, val: APPLICATION_ID },
-        lang: { dir: oracledb.BIND_IN, val: LANGUAGE },
-        eligibility: { dir: oracledb.BIND_OUT, type: oracledb.DB_TYPE_CURSOR },
-      };
-      return tx.raw('begin OA_PKG_UTIL.GET_TF_ELIGIBILITY(:app_id, :lang, :eligibility); end;', bindVars);
-    }, { connection: con });
-    const result = await response[0].getRow();
-    response[0].close();
-    return result;
+    return await this.getEligibilityImpl('begin OA_PKG_UTIL.GET_TF_ELIGIBILITY(:app_id, :lang, :eligibility); end;', APPLICATION_ID, LANGUAGE);
   }
 
   async getGaEligibility(APPLICATION_ID, LANGUAGE) {
+    return await this.getEligibilityImpl('begin OA_PKG_UTIL.GET_GA_ELIGIBILITY(:app_id, :lang, :eligibility); end;', APPLICATION_ID, LANGUAGE);
+  }
+
+  async getEligibilityImpl(QUERY, APPLICATION_ID, LANGUAGE) {
     const con = await this.knex.client.pool.acquire().promise;
     const response = await this.knex.client.transaction(async (tx) => {
       const bindVars = {
@@ -55,7 +27,7 @@ class EligibilityCalcsDao extends SQLDataSource {
         lang: { dir: oracledb.BIND_IN, val: LANGUAGE },
         eligibility: { dir: oracledb.BIND_OUT, type: oracledb.DB_TYPE_CURSOR },
       };
-      return tx.raw('begin OA_PKG_UTIL.GET_GA_ELIGIBILITY(:app_id, :lang, :eligibility); end;', bindVars);
+      return tx.raw(QUERY, bindVars);
     }, { connection: con });
     const result = await response[0].getRow();
     response[0].close();
