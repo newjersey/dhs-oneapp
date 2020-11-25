a5f19d9dfc44cc7b6d9a9bcc8df9195816cc2e32
Maya <maya@USDSs-MBP.fios-router.home>
2020-11-25 14:03:23 -0500

    add graphql validation

--- oneapp-server/types/ApplicationContact.js
+++ oneapp-server/types/ApplicationContact.js
@@ -8,55 +8,55 @@ const typeDef = gql`
 
   type ApplicationContact {
     "Applicant first name"
-    APPLICANT_FIRST_NAME: String
+    APPLICANT_FIRST_NAME: String! @constraint(pattern: "^[a-zA-Z.''-'\s]*$", maxLength: 25)
     "Applicant last name"
-    APPLICANT_LAST_NAME: String
+    APPLICANT_LAST_NAME: String! @constraint(pattern: "^[a-zA-Z.''-'\s]*$", maxLength: 25)
     "Applicant middle name"
-    APPLICANT_MIDDLE_NAME: String
+    APPLICANT_MIDDLE_NAME: String @constraint(pattern: "^[a-zA-Z.''-'\s]*$", maxLength: 25)
     "Application maiden name"
-    APPLICANT_MAIDEN_NAME: String
+    APPLICANT_MAIDEN_NAME: String @constraint(pattern: "^[a-zA-Z.''-'\s]*$", maxLength: 25)
     "Payee first name, if different than applicant"
-    PAYEE_FIRST_NAME: String
+    PAYEE_FIRST_NAME: String! @constraint(pattern: "^[a-zA-Z.''-'\s]*$", maxLength: 25)
     "Payee last name, if different than applicant"
-    PAYEE_LAST_NAME: String
+    PAYEE_LAST_NAME: String! @constraint(pattern: "^[a-zA-Z.''-'\s]*$", maxLength: 25)
     "Is applicant homeless"
     IS_HOMELESS: Boolean
     "Applicant address line 1"
-    ADDRESS1: String
+    ADDRESS1: String @constraint(pattern: "^.*(?=.*[a-zA-Z])([a-zA-Z0-9'#@&/-_.,\s])*$", maxLength: 50)
     "Applicant address line 2"
-    ADDRESS2: String
+    ADDRESS2: String @constraint(pattern: "^[a-zA-Z0-9.,'#@&\-_/\s]*$", maxLength: 50)
     "Applicant address city"
-    CITY: String
+    CITY: String @constraint(pattern: "^[a-zA-Z0-9.,'#@&\-_/\s]*$", maxLength: 50)
     "Applicant address state"
     STATE: String
     "Applicant address zipcode"
-    ZIP: Int
+    ZIP: Int @constraint(maxLength: 5)
     "Applicant address zipcode extend 4"
     ZIP4: Int
     "New Jersey county id, retrieved from zipcode to county lookup"
-    COUNTY_NUMBER: Int
+    COUNTY_NUMBER: Int!
     "Mailing address line 1"
-    M_ADDRESS1: String
+    M_ADDRESS1: String @constraint(pattern: "^.*(?=.*[a-zA-Z])([a-zA-Z0-9'#@&/-_.,\s])*$", maxLength: 50)
     "Mailing address line 2"
-    M_ADDRESS2: String
+    M_ADDRESS2: String @constraint(pattern: "^[a-zA-Z0-9.,'#@&\-_/\s]*$", maxLength: 50)
     "Mailing address city"
-    M_CITY: String
+    M_CITY: String @constraint(pattern: "^[a-zA-Z0-9.,'#@&\-_/\s]*$", maxLength: 50)
     "Mailing address state"
     M_STATE: String
     "Mailing address zipcode"
-    M_ZIP: Int
+    M_ZIP: Int @constraint(maxLength: 5)
     "Mailing address zipcode extended 4"
-    M_ZIP4: Int
+    M_ZIP4: Int @constraint(maxLength: 4)
     "Applicant home phone number"
-    HOME_PHONE_NUMBER: Int
+    HOME_PHONE_NUMBER: Int @constraint(pattern: "^(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?\d{4}$", maxLength: 12)
     "Applicant work phone number"
-    WORK_PHONE_NUMBER: Int
+    WORK_PHONE_NUMBER: Int @constraint(pattern: "^(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?\d{4}$", maxLength: 12)
     "Applicant cell phone number"
-    CELL_PHONE_NUMBER: Int
+    CELL_PHONE_NUMBER: Int @constraint(pattern: "^(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?\d{4}$", maxLength: 12)
     "Applicant other phone number"
-    OTHER_PHONE_NUMBER: Int
+    OTHER_PHONE_NUMBER: Int @constraint(pattern: "^(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?\d{4}$", maxLength: 12)
     "Applicant email address"
-    EMAIL_ADDRESS: String
+    EMAIL_ADDRESS: String @constraint(format: "email", maxLength: 50)
     "Does applicant not have a phone number"
     NO_PHONE_NUMBER: Boolean
     "Does applicant not have contact information"
@@ -69,55 +69,55 @@ const typeDef = gql`
 
   input ApplicationContactInput {
     "Applicant first name"
-    APPLICANT_FIRST_NAME: String
+    APPLICANT_FIRST_NAME: String! @constraint(pattern: "^[a-zA-Z.''-'\s]*$", maxLength: 25)
     "Applicant last name"
-    APPLICANT_LAST_NAME: String
+    APPLICANT_LAST_NAME: String! @constraint(pattern: "^[a-zA-Z.''-'\s]*$", maxLength: 25)
     "Applicant middle name"
-    APPLICANT_MIDDLE_NAME: String
+    APPLICANT_MIDDLE_NAME: String @constraint(pattern: "^[a-zA-Z.''-'\s]*$", maxLength: 25)
     "Application maiden name"
-    APPLICANT_MAIDEN_NAME: String
+    APPLICANT_MAIDEN_NAME: String @constraint(pattern: "^[a-zA-Z.''-'\s]*$", maxLength: 25)
     "Payee first name, if different than applicant"
-    PAYEE_FIRST_NAME: String
+    PAYEE_FIRST_NAME: String! @constraint(pattern: "^[a-zA-Z.''-'\s]*$", maxLength: 25)
     "Payee last name, if different than applicant"
-    PAYEE_LAST_NAME: String
+    PAYEE_LAST_NAME: String! @constraint(pattern: "^[a-zA-Z.''-'\s]*$", maxLength: 25)
     "Is applicant homeless"
     IS_HOMELESS: Boolean
     "Applicant address line 1"
-    ADDRESS1: String
+    ADDRESS1: String @constraint(pattern: "^.*(?=.*[a-zA-Z])([a-zA-Z0-9'#@&/-_.,\s])*$", maxLength: 50)
     "Applicant address line 2"
-    ADDRESS2: String
+    ADDRESS2: String @constraint(pattern: "^[a-zA-Z0-9.,'#@&\-_/\s]*$", maxLength: 50)
     "Applicant address city"
-    CITY: String
+    CITY: String @constraint(pattern: "^[a-zA-Z0-9.,'#@&\-_/\s]*$", maxLength: 50)
     "Applicant address state"
     STATE: String
     "Applicant address zipcode"
-    ZIP: Int
+    ZIP: Int @constraint(maxLength: 5)
     "Applicant address zipcode extend 4"
     ZIP4: Int
     "New Jersey county id, retrieved from zipcode to county lookup"
-    COUNTY_NUMBER: Int
+    COUNTY_NUMBER: Int!
     "Mailing address line 1"
-    M_ADDRESS1: String
+    M_ADDRESS1: String @constraint(pattern: "^.*(?=.*[a-zA-Z])([a-zA-Z0-9'#@&/-_.,\s])*$", maxLength: 50)
     "Mailing address line 2"
-    M_ADDRESS2: String
+    M_ADDRESS2: String @constraint(pattern: "^[a-zA-Z0-9.,'#@&\-_/\s]*$", maxLength: 50)
     "Mailing address city"
-    M_CITY: String
+    M_CITY: String @constraint(pattern: "^[a-zA-Z0-9.,'#@&\-_/\s]*$", maxLength: 50)
     "Mailing address state"
     M_STATE: String
     "Mailing address zipcode"
-    M_ZIP: Int
+    M_ZIP: Int @constraint(maxLength: 5)
     "Mailing address zipcode extended 4"
-    M_ZIP4: Int
+    M_ZIP4: Int @constraint(maxLength: 4)
     "Applicant home phone number"
-    HOME_PHONE_NUMBER: Int
+    HOME_PHONE_NUMBER: Int @constraint(pattern: "^(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?\d{4}$", maxLength: 12)
     "Applicant work phone number"
-    WORK_PHONE_NUMBER: Int
+    WORK_PHONE_NUMBER: Int @constraint(pattern: "^(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?\d{4}$", maxLength: 12)
     "Applicant cell phone number"
-    CELL_PHONE_NUMBER: Int
+    CELL_PHONE_NUMBER: Int @constraint(pattern: "^(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?\d{4}$", maxLength: 12)
     "Applicant other phone number"
-    OTHER_PHONE_NUMBER: Int
+    OTHER_PHONE_NUMBER: Int @constraint(pattern: "^(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?\d{4}$", maxLength: 12)
     "Applicant email address"
-    EMAIL_ADDRESS: String
+    EMAIL_ADDRESS: String @constraint(format: "email", maxLength: 50)
   }
 
   enum ApplicationType {
