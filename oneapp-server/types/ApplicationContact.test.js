6563ea6c35c0552c6df0694108297ae20e868741
Maya <maya@USDSs-MBP.fios-router.home>
2020-12-08 16:49:17 -0500

    add basic mutation and query tests

---
+++ oneapp-server/types/ApplicationContact.test.js
@@ -0,0 +1,42 @@
+const { createTestClient, dataSources, services } = require('../__utils/TestingUtils');
+const client = createTestClient();
+const passwordGenerator = require('generate-password');
+
+jest.mock('generate-password');
+
+describe('application contact query', () => {
+  it('updates contact info', async () => {
+    const user = {USER_ID: 'user123'};
+    const authClient = createTestClient({user});
+
+    dataSources.ApplicationContactDao.getContact.mockReturnValue({
+      APPLICANT_FIRST_NAME: "Test",
+      APPLICANT_LAST_NAME: "Test",
+      PAYEE_FIRST_NAME: "Test",
+      PAYEE_LAST_NAME: "Test",
+      IS_HOMELESS: true,
+      COUNTY_NUMBER: 1,
+    });
+
+    const query = `
+      {
+        application {
+          contact { 
+            APPLICANT_FIRST_NAME
+            IS_HOMELESS
+            COUNTY_NUMBER
+          }
+        }
+      } 
+    `;
+    const response = await authClient.query({ query });
+    expect(response.data.application.contact.APPLICANT_FIRST_NAME).toEqual('Test');
+    expect(response.data.application.contact.IS_HOMELESS).toEqual(true);
+    expect(response.data.application.contact.COUNTY_NUMBER).toEqual(1);
+
+    expect(dataSources.ApplicationContactDao.getContact).toHaveBeenCalledWith(
+      "user123",
+      0,
+    );
+  });
+});
